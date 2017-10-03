df792d1c6af46c29b44d74e4ab3f2df2b2684bb2
ozzonfire <catalinmolnar@yahoo.com>
2017-10-03 19:38:13 +0300

    03oct_1

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,54 @@ app.controller("MainController", function($scope){
 
     $scope.movies = [
         {
-            title: "Game of Thrones",
-            year: 2011,
-			duration: '1h 00min',
-			genre: 'Adventure, Drama, Fantasy',
-            actors: 'Actor One',
-			description: "Nine noble families fight for control over the mythical lands of Westeros, while a forgotten race returns after being dormant for thousands of years",
-			poster: 'posters/16972.jpg',
-			trailer: 'giYeaKsXnsI'
+            title: "2 Guns",
+            year: 2013,
+			duration: '1h 49min',
+			genre: 'Action, Comedy, Crime',
+            actors: 'Denzel Washington, Mark Wahlberg, Paula Patton',
+			description: "Two hardened criminals get into trouble with the US border patrol after meeting with a Mexican drug lord, and then revelations start to unfold.",
+			poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNTQ5MTgzNDg4OF5BMl5BanBnXkFtZTcwMjAyODEzOQ@@._V1_SY1000_CR0,0,630,1000_AL_.jpg',
+			trailer: 'AK6EbfdnTHg'
+        },
+        {
+            title: "3 10 to Yuma",
+            year: 2007,
+			duration: '2h 02min',
+			genre: 'Adventure, Crime, Drama',
+            actors: 'Russell Crowe, Christian Bale, Ben Foster',
+			description: "TA small-time rancher agrees to hold a captured outlaw who's awaiting a train to go to court in Yuma. A battle of wills ensues as the outlaw tries to psych out the rancher.",
+			poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BODE0NTcxNTQzNF5BMl5BanBnXkFtZTcwMzczOTIzMw@@._V1_SY1000_CR0,0,663,1000_AL_.jpg',
+			trailer: 'jX1m45CwvJ8&t=3s'
+        },
+        {
+            title: "3 Idiots",
+            year: 2009,
+			duration: '2h 44min',
+			genre: 'Adventure, Comedy, Drama',
+            actors: 'Aamir Khan, Madhavan, Mona Singh',
+			description: "Two friends are searching for their long lost companion. They revisit their college days and recall the memories of their friend who inspired them to think differently, even as the rest of the world called them 'idiots'.",
+			poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BZWRlNDdkNzItMzhlZC00YTdmLWIwNjktYjY5NjQ1ZmQ3N2FkXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,747,1000_AL_.jpg',
+			trailer: 'K0eDlFX9GMc'
         }
+
+
+
+
+
+
+        /*
+        ,
+        {
+            title: "",
+            year: 2007,
+			duration: '1h min',
+			genre: '',
+            actors: '',
+			description: "",
+			poster: '',
+			trailer: ''
+        }
+        */
 
     ];
 	
